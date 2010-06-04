9afddc74635f9466528b55a30b36334e3b6d7d8c
DFectuoso <santiago1717@gmail.com>
2010-06-03 21:52:28 -0500

    Added comments, removed emails(now nicknames) and ordered on the date

--- main.py
+++ main.py
@@ -22,7 +22,33 @@ class Confirm(db.Model):
     user = db.UserProperty(auto_current_user_add=True)
     hackup = db.ReferenceProperty(HackUp)
 
+class Comment(db.Model):
+    user = db.UserProperty(auto_current_user_add=True)
+    hackup = db.ReferenceProperty(HackUp)
+    text = db.StringProperty(required=True, multiline=True)
 # Handlers:
+
+class CommentHandler(webapp.RequestHandler):
+    def post(self,id):
+       user = users.get_current_user()
+       idInt = 0
+    ##   try: 
+       idInt = int(id)
+       hackup = HackUp.get_by_id(idInt)
+       comment = Comment(
+          user=user,
+          hackup=hackup,
+          text=self.request.get("comment")
+       )
+       comment.put()
+       if (hackup):
+         self.redirect('/view/' + id)
+       else:
+         self.redirect('/')
+##       except:
+  ##       self.redirect('/')
+
+
 class ViewHandler(webapp.RequestHandler):
     def get(self,id):
        user = users.get_current_user()
@@ -31,6 +57,7 @@ class ViewHandler(webapp.RequestHandler):
          idInt = int(id)
          hackup = HackUp.get_by_id(idInt)
          confirmList = Confirm.all().filter("hackup =", hackup)
+         commentList = Comment.all().filter("hackup =", hackup)
          confirmed = Confirm.all().filter("hackup =", hackup).filter("user =", user).fetch(100)
          if (hackup):
            self.response.out.write(template.render('templates/view.html', locals()))
@@ -77,7 +104,7 @@ class MainHandler(webapp.RequestHandler):
             logout_url = users.create_logout_url('/')
         else:
             login_url = users.create_login_url('/')
-        hackups = HackUp.all().filter("when >=", datetime.date.today() - datetime.timedelta(days=1))
+        hackups = HackUp.all().filter("when >=", datetime.date.today() - datetime.timedelta(days=1)).order("when")
         self.response.out.write(template.render('templates/main.html', locals()))
     
 def main():
@@ -85,6 +112,7 @@ def main():
         ('/', MainHandler), #This list all the upcoming hackups and has a link to create a new one
         ('/view/(.+)', ViewHandler), #This allows to see details of the hackup, who is going, and confirm
         ('/confirm/(.+)', ConfirmHandler), #This allows to confirm going to a hackup
+        ('/comment/(.+)', CommentHandler), #This allows to comment on the hackup
         ('/create', CreateHandler), #This allows to create a new thing
       ], debug=True)
     util.run_wsgi_app(application)
