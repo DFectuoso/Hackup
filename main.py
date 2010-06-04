1b9d8e28b900488aa46805acb8c50baab3ca470b
DFectuoso <santiago1717@gmail.com>
2010-06-03 20:09:45 -0500

    Create, list and almost view working

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import logging
 import urllib
 import urllib2
 import hashlib
- 
+import time
+import datetime
+
 from google.appengine.api import mail
 from google.appengine.ext import webapp, db
 from google.appengine.api import urlfetch, memcache, users
@@ -14,23 +16,34 @@ from django.template.defaultfilters import timesince
 class HackUp(db.Model):
     user = db.UserProperty(auto_current_user_add=True)
     details = db.StringProperty(required=True, multiline=True)
-    address = db.StringProperty(required=True, multiline=True)
-    created = db.DateTimeProperty(auto_now_add=True)
     when = db.DateTimeProperty(auto_now_add=True)
 
 # Handlers:
 class ViewHandler(webapp.RequestHandler):
     def get(self,id):
        user = users.get_current_user()
-       self.response.out.write(template.render('templates/view.html', locals()))
+       idInt = 0
+       try: 
+         idInt = int(id)
+         hackup = HackUp.get_by_id(idInt)
+         if (hackup):
+           self.response.out.write(template.render('templates/view.html', locals()))
+         else:
+           self.redirect('/')
+       except:
+         self.redirect('/')
 
 class CreateHandler(webapp.RequestHandler):
     def get(self):
        user = users.get_current_user()
        self.response.out.write(template.render('templates/create.html', locals()))
        
-    def post(self, update_id):
+    def post(self):
        user = users.get_current_user()
+       t = time.strptime(self.request.get("when"), "%d/%m/%Y")
+       dt = datetime.datetime(*t[0:5])
+       hackup = HackUp(user=user, details=self.request.get("descripcion","El fail user no escribio nada fail"), when=dt)
+       hackup.put()
        self.redirect('/')
 
 class ConfirmHandler(webapp.RequestHandler):
@@ -45,6 +58,7 @@ class MainHandler(webapp.RequestHandler):
             logout_url = users.create_logout_url('/')
         else:
             login_url = users.create_login_url('/')
+        hackups = HackUp.all().filter("when >", datetime.datetime.now())
         self.response.out.write(template.render('templates/main.html', locals()))
     
 def main():
